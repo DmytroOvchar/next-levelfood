0f5b68fcfa3eefe9c9771d0bad2358a86abb819f
DmytroOvchar <ov4ar.dmytro@gmail.com>
2024-02-29 21:58:32 +0200

    added slider

--- app/page.js
+++ app/page.js
@@ -1,11 +1,16 @@
 import Link from "next/link";
 import classes from "./page.module.css";
 
+import SlideShow from "../components/SlideShow/SlideShow";
+
 export default function Home() {
+  console.log("Executing");
   return (
     <>
       <header className={classes.header}>
-        <div className={classes.slideshow}></div>
+        <div className={classes.slideshow}>
+          <SlideShow />
+        </div>
         <div>
           <div className={classes.hero}>
             <h1>NextLevel Food for NextLevel Foodies</h1>
